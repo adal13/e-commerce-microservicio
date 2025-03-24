5a58f16b852eef132e51ff9a141527dc7a416efa
Adal, Rommel, Elsa <adal13gomez.agg@gmail.com>
2025-03-23 22:06:04 -0600

    Proyecto inicial de microservicio-clientes

---
+++ microservicio-clientes/src/test/java/com/adal/clientes/MicroservicioClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adal.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

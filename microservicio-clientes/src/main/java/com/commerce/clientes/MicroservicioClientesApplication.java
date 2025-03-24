c2bf116844cb0e2375f8e2f6a148bb501d8cb210
Adal, Rommel, Elsa <adal13gomez.agg@gmail.com>
2025-03-24 08:23:51 -0600

    Refactorizando el nombre del dominio de com.adal a com.commerce

--- microservicio-clientes/src/main/java/com/commerce/clientes/MicroservicioClientesApplication.java
+++ microservicio-clientes/src/main/java/com/commerce/clientes/MicroservicioClientesApplication.java
@@ -1,4 +1,4 @@
-package com.adal.clientes;
+package com.commerce.clientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

10385b209f49a22decaafa696f48b2ac64d05700
Adal, Rommel, Elsa <adal13gomez.agg@gmail.com>
2025-03-23 22:07:05 -0600

    Proyecto inicial de microservicio-productos

---
+++ microservicio-productos/src/test/java/com/adal/productos/MicroservicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adal.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

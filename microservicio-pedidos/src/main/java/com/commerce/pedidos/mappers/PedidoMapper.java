357cb458365eb6eb78d0f807d96f0e5be3ac5aa1
Adal, Rommel, Elsa <adal13gomez.agg@gmail.com>
2025-03-25 12:14:37 -0600

    Crendo mappers para poder pasar de entity a dto y de dto a entity

---
+++ microservicio-pedidos/src/main/java/com/commerce/pedidos/mappers/PedidoMapper.java
@@ -0,0 +1,59 @@
+package com.commerce.pedidos.mappers;
+
+import java.util.Optional;
+
+import javax.print.attribute.standard.MediaSize.Other;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.commerce.commons.models.entity.Cliente;
+import com.commerce.commons.models.entity.Pedidos;
+import com.commerce.commons.models.entity.Productos;
+import com.commerce.pedidos.clients.PedidoClient;
+import com.commerce.pedidos.controllers.PedidoControllers;
+import com.commerce.pedidos.dto.PedidoDTO;
+import com.commerce.pedidos.dto.ProductoDTO;
+
+@Component
+public class PedidoMapper {
+	
+	@Autowired
+	private PedidoClient pedidoClient;
+	
+	public PedidoDTO entityToDto(Pedidos pedido) {
+		if(pedido == null) {
+			return null;
+		}
+		
+		PedidoDTO pedidoDTO = new PedidoDTO();
+		pedidoDTO.setClienteId(pedido.getCliente() != null ? pedido.getCliente().getId() : null );
+		pedidoDTO.setTotal(pedido.getTotal());
+		pedidoDTO.setFechaCreacion(pedido.getFechaCreacion());
+		pedidoDTO.setIdEstado(pedido.getIdEstado());
+		pedidoDTO.setProductosIds(pedido.getProductos() != null ? ((Cliente) pedido.getProductos()).getId() : null);
+		return pedidoDTO;
+	}
+	
+	public Pedidos dtoToEntity(PedidoDTO pedidoDTO) {
+		if(pedidoDTO == null) {
+			return null;
+		}
+		
+		Pedidos pedido = new Pedidos();
+		pedido.setTotal(pedidoDTO.getTotal());
+		pedido.setFechaCreacion(pedidoDTO.getFechaCreacion());
+		pedido.setIdEstado(pedidoDTO.getIdEstado()); 
+		
+		if (pedidoDTO.getClienteId() != null) {
+			
+			 Cliente cliente = new Cliente();
+		        cliente.setId(pedidoDTO.getClienteId());
+		        pedido.setCliente(cliente);
+		}
+		
+		return pedido;
+	}
+	
+	
+}
